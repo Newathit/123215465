92fc396a51993435371578a42eb128cbad84a2b7
Athit Sangyoo <646051200061@mail.rmutk.ac.th>
2023-02-14 17:31:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/logregis/kal.kt
@@ -0,0 +1,11 @@
+package com.example.logregis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class kal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kal)
+    }
+}
